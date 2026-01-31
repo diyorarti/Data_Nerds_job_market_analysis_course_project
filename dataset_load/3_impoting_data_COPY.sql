e5a384d56b3d187551c48dbc03faf4f57e84f8c9
diyorarti <diyorarti@gmail.com>
2026-01-31 11:06:29 +0100

    DB ready

--- dataset_load/3_impoting_data_COPY.sql
+++ dataset_load/3_impoting_data_COPY.sql
@@ -4,3 +4,8 @@ Importing job_postings.csv dataset using COPY
 COPY job_postings
 FROM 'D:\Users\User\SQL\Data_Nerds_job_market_analysis_course_project\dataset\job_postings.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- checking table
+SELECT *
+FROM job_postings
+LIMIT 10;;
